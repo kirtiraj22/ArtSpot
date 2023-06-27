b2f0714abaa8a9215238707bddb7d4f4cd7f0119
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-06-27 21:52:18 +0530

    modified authprovider

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -9,7 +9,7 @@ type Provider = {
   type: string;
   signinUrl: string;
   callbackUrl: string;
-  signinUrlParams: Record<string,string> | null;
+  signinUrlParams?: Record<string,string> | null;
 }
 
 type Providers = Record<string,Provider>;
@@ -17,9 +17,25 @@ type Providers = Record<string,Provider>;
 const AuthProviders = () => {
   const [providers, setProviders] = useState<Providers | null>(null);
 
-  return (
-    <div>AuthProviders</div>
-  )
+  useEffect(() => {
+    const fetchProviders = async() => {
+      const res = await getProviders();
+      console.log(res);
+      setProviders(res);
+    }
+    fetchProviders();
+  },[])
+
+  if(providers){
+    return(
+      <div>
+        {
+          Object.values(providers).map((provider: Provider, i) =>
+          <button key={i} onClick={()=> signIn(provider?.id)}>{provider.id}</button>
+        )}
+      </div>
+    )
+  }
 }
 
 export default AuthProviders
